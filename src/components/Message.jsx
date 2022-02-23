84c41e383e0e6feafc39cc88f5ca74ca821aa269
Fernando Tesone <fenatesone@gmail.com>
2022-02-23 00:09:49 -0300

    Solve Message component test

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,4 +1,4 @@
-/*
+/**
  * The Message component contains an anchor element and
  * a paragraph below the anchor. Rendering of the paragraph
  * should be toggled by clicking on the anchor element
@@ -9,12 +9,15 @@
  * - After another click, the paragraph should not be rendered.
  * - Finish the Message component by implementing this logic.
  */
+import { useState } from "react";
 
 export function Message() {
+	const [display, setDisplay] = useState(false);
+
 	return (
 		<>
-			<a href="#">Want to buy a new car?</a>
-			<p>Call +11 22 33 44 now!</p>
+			<a href="#" onClick={() => setDisplay(prevDisplay => !prevDisplay)}>Want to buy a new car?</a>
+			{display && <p>Call +11 22 33 44 now!</p>}
 		</>
 	);
 }
