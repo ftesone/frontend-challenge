50eed1c0e52889b37099ee9449c03a945ed30c6f
Fernando Tesone <fenatesone@gmail.com>
2022-02-23 00:47:09 -0300

    Solve ImageGallery test

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -6,20 +6,27 @@
  * It should also implement the following logic:
  * - When the button is clicked, the image that is in the same div as the button should be removed from the gallery.
  */
+import {createRef, forwardRef} from "react";
 
-function Image({ src, onRemove }) {
+function Image({ src, onRemove, forwardedRef }) {
 	return (
-		<div class="image">
-			<img />
-			<button class="remove">X</button>
+		<div className="image" ref={forwardedRef}>
+			<img src={src} />
+			<button className="remove" onClick={() => onRemove(forwardedRef)}>X</button>
 		</div>
 	);
 }
 
+const ImageRef = forwardRef((props, ref) => <Image {...props} forwardedRef={ref} />);
+
 export function ImageGallery({ links }) {
+	const parentRef = createRef();
+
+	const removeImage = ref => parentRef.current.removeChild(ref.current);
+
 	return (
-		<div>
-			{/* Implement here the Image Gallery using <Image /> component */}
+		<div ref={parentRef}>
+			{links && links.map(link => <ImageRef key={link} src={link} onRemove={removeImage} ref={createRef()} />)}
 		</div>
 	);
 }
